0849637470f810c1e034c2a471e6f18aab3d56b1
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-25 09:44:25 -0800

    Creating Produto class

---
+++ ConsumindoAPI/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsumindoAPI;
+
+public class Produto
+{
+    public int ProdutoId { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string ImagemUrl { get; set; }
+    public int CategoriaId { get; set; }
+}
