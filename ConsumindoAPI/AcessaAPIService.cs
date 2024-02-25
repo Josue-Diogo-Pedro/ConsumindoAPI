0715f471134aff2a0df5f132413f4521ad66a0f9
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-25 09:54:38 -0800

    Get all produtos

--- ConsumindoAPI/AcessaAPIService.cs
+++ ConsumindoAPI/AcessaAPIService.cs
@@ -1,6 +1,41 @@
-﻿namespace ConsumindoAPI;
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+
+namespace ConsumindoAPI;
 
 public class AcessaAPIService
 {
+    public async Task<List<Produto>> GetAllProdutos(string URI, AccessToken accessToken)
+    {
+        using(var client = new HttpClient())
+        {
+            GetHeaderTokenAuthorization(client, accessToken);
 
+            //GET api/produtos
+            using (var response = await client.GetAsync(URI))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var produtoJsonString = await response.Content.ReadAsStringAsync();
+                    List<Produto> produtos = JsonConvert.DeserializeObject<Produto[]>(produtoJsonString).ToList();
+                    return produtos;
+                }
+                else
+                {
+                    throw new Exception("Não foi possível obter os produtos: " + response.StatusCode);
+                }
+            }
+        }
+    }
+
+    public static void GetHeaderTokenAuthorization(HttpClient client, AccessToken accessToken)
+    {
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", accessToken.Token);
+    }
 }
