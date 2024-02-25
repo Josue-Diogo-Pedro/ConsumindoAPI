5eed2d5f729591a3ef6d44d607402d1f34576acf
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-25 10:02:49 -0800

    Btn GetAllProdutos implemented

--- ConsumindoAPI/Form1.Designer.cs
+++ ConsumindoAPI/Form1.Designer.cs
@@ -83,6 +83,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "Retomar Produtos";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
--- ConsumindoAPI/Form1.cs
+++ ConsumindoAPI/Form1.cs
@@ -7,8 +7,28 @@ public partial class Form1 : Form
         InitializeComponent();
     }
 
+    string URI = "";
+    int codigoProduto = 1;
+    private static string _urlBase;
+    private static AccessToken accessToken;
+
     private void Form1_Load(object sender, EventArgs e)
     {
 
+    }
+
+    private async void button2_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            URI = textBox1.Text;
+            var acessoAPI = new AcessaAPIService();
+            List<Produto> produtos = await acessoAPI.GetAllProdutos(URI, accessToken);
+            dataGridView1.DataSource = produtos;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Erro : " + ex.Message);
+        }
     }
 }
